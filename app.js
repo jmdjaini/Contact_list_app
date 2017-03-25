873681916cbbd1dc76eaff5d83c43225d89d7420
jmdjaini <jmdjaini@gmail.com>
2017-03-25 18:41:11 +0530

    Addding source code

---
+++ app.js
@@ -0,0 +1,59 @@
+// MEAN Stack RESTful API Tutorial - Contact List App
+//NODE JS CODE
+/* MODULE DEPENDENCIES       */
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
+/**ADDING STATIC FILES */
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+/*MAIN CODE*/
+//SHOWING THE CURRENT CONTACT LIST
+app.get('/contactlist', function (req, res) {
+  console.log('I received a GET request');
+
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+//INSERTING NEW CONTACT IN MONGO DB
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+//DELETING ANY CONTACT FROM THE CONTACT LIST
+app.delete('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+//FINDING CONTACT FROM THE CONTACT LIST
+app.get('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+//UPDATING ANY CONTACT FROM THE CONTACT LIST
+app.put('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.contactlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+/*SERVER LISTENING*/
+app.listen(4000);
+console.log("Server running on port 3000");
